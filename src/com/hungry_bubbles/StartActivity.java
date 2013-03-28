8bc92a2f819686990007c157c99b27666fe12ebc
Tim Heard <tjh2430@rit.edu>
2013-03-28 12:05:46 -0500

    Removed a TODO from StartActivity.java

--- src/com/hungry_bubbles/StartActivity.java
+++ src/com/hungry_bubbles/StartActivity.java
@@ -3,7 +3,6 @@ package com.hungry_bubbles;
 import android.app.Activity;
 import android.os.Bundle;
 
-// TODO: Remove file if this class is not used
 public class StartActivity extends Activity {
 
     @Override
